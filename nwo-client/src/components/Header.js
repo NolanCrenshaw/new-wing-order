5beda8538a299fcbafdecac2cf884abaf153f75f
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-11-30 14:02:31 -0500

    styling work --incomplete

--- nwo-client/src/components/Header.js
+++ nwo-client/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className="header-container">
             <nav>
-                <div className="header__logo-container">
+                <div>
                     <img src="https://nwobucky.s3.us-east-2.amazonaws.com/nwo_images/NWO_Head+Logo_Full+Color_White.png" alt="md-Logo-head-white" />
                     <img src="https://nwobucky.s3.us-east-2.amazonaws.com/nwo_images/NWO_Ver+Text+Logo_White.png" alt="md-Logo-text-white" />
                 </div>
--- nwo-client/src/pages/Main.js
+++ nwo-client/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../components/Header';
 
 
 const Main = () => {
@@ -6,6 +7,7 @@ const Main = () => {
 
     return (
         <div className="main-container">
+            <Header/>
             <h1>Hello World</h1>
             {/* <div className="main">
                 <div className="hero-logo--container">
