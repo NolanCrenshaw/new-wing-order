e59a79b5290a8a8e06631e3d7411354ae5fca534
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-11-30 22:13:20 -0500

    location ele base created

--- nwo-client/src/components/Main.js
+++ nwo-client/src/components/Main.js
@@ -3,23 +3,9 @@ import React from "react";
 const Main = () => {
   return (
     <div className="main-container">
-      {/* <div className="bulk-image--container">
-        <img
-          id="bulk-image"
-          src="https://nwobucky.s3.us-east-2.amazonaws.com/nwo_images/a1.jpg"
-          alt="NWO"
-        />
-      </div> */}
-      {/* <img
-        id="menu"
-        src="https://nwobucky.s3.us-east-2.amazonaws.com/nwo_images/NWO+Menu_Side1.jpg"
-        alt="NWO Menu"
-      />
-      <img
-        id="awards"
-        src="https://nwobucky.s3.us-east-2.amazonaws.com/nwo_images/NWO+Menu_Side2.jpg"
-        alt="NWO Awards"
-      /> */}
+      <div id="location-container">
+        <p>Hello World</p>
+      </div>
     </div>
   );
 };
