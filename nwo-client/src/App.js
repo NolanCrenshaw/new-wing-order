93eaf6c471f4c115bd43e78219d4103b38c1491e
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-11-30 22:38:52 -0500

    location component absorbed by Main.js

--- nwo-client/src/App.js
+++ nwo-client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
@@ -8,7 +9,9 @@ const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <Main />
+      <Router>
+        <Main />
+      </Router>
       <Footer />
     </div>
   );
--- nwo-client/src/components/Main.js
+++ nwo-client/src/components/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Location from "./Location";
 
 const Main = () => {
   return (
     <div className="main-container">
-      <Location />
+      <div className="location-container">
+        <p>Hello World</p>
+      </div>
     </div>
   );
 };
