38788b20ce142dd427df3c9307438163b7bcc91b
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-02 02:29:01 -0500

    error fix

--- nwo-client/src/components/Main.js
+++ nwo-client/src/components/Main.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Geocode from "react-geocode";
 
 const Main = () => {
-  // Leaflet
+  // State Hooks
+  const [location, setLocation] = useState("Memphis, TN");
+  const [address, setAddress] = useState("default");
+  const [latitude, setLatitude] = useState(35.1295);
+  const [longitude, setLongitude] = useState(-89.8721);
+
+  // Geocode
+  Geocode.setLanguage("en");
+
+  const InterpretLocation = async () => {
+    await Geocode.fromAddress("Memphis, TN").then(
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+        setLatitude(lat);
+        setLongitude(lng);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  };
+
+  useEffect(() => {
+    InterpretLocation();
+  }, []);
 
   return (
     <div className="main-container">
       <div className="location-container">
         <div id="header-box">
-          <h1>Location</h1>
-          <h3>Address</h3>
+          <h1>{location}</h1>
+          <h3>{address}</h3>
         </div>
         <div id="map-box">
           <MapContainer
-            center={[51.505, -0.09]}
-            zoom={13}
+            center={[`${latitude}`, `${longitude}`]}
+            zoom={10}
             scrollWheelZoom={false}
           >
             <TileLayer
