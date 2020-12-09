4c6e45af302dd73602df150bb1622702da6f1e80
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-08 20:05:54 -0500

    main page styling work

--- nwo-client/src/components/Store.js
+++ nwo-client/src/components/Store.js
@@ -4,6 +4,10 @@ const Store = () => {
   return (
     <div className="store-container">
       <p>Store</p>
+      <img
+        alt="truck_drawing"
+        src="https://nwobucky.s3.us-east-2.amazonaws.com/nwo_images/nwo_png_truck_drawing.png"
+      />
     </div>
   );
 };
