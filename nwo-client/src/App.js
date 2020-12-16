c5fb9acb27796c1fca8ec11ad76c18a50d9741ec
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-15 19:10:01 -0500

    client config alter

--- nwo-client/src/App.js
+++ nwo-client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Loading from "./components/Loading";
 import Landing from "./components/Landing";
