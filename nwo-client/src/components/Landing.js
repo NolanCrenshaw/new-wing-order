de2b61199949d4880765a25e61aba5b9a54b5dc5
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-23 18:52:13 -0500

    landing sizing adjustments

--- nwo-client/src/components/Landing.js
+++ nwo-client/src/components/Landing.js
@@ -7,15 +7,11 @@ const Landing = () => {
     <div className="landing-container">
       <div className="splash-container">
         <div id="splash_logo">
+          <h1>Come get the</h1>
+          <h1>Best Wings in Memphis</h1>
           <img alt="splash logo" src="./images/head_white.png" />
         </div>
-        <Location />
-        {/* <div id="splash_picture">
-          <h1>Come Visit for the Best Wings in Memphis</h1>
-          <div>
-            <img src="./images/truck_slice_01.jpg" />
-          </div>
-        </div> */}
+        <Location />s{" "}
       </div>
     </div>
   );
