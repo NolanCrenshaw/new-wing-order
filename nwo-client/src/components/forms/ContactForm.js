16405ef113b02cea587b5143bf0c67f8215f410d
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-04 13:55:55 -0500

    emailjs fully integrated

--- nwo-client/src/components/forms/ContactForm.js
+++ nwo-client/src/components/forms/ContactForm.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
+  const userID = process.env.REACT_APP_EMAILJS_USER_ID;
+  const ejsSERVICE = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const ejsTEMPLATE = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
   // State
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
@@ -22,50 +26,74 @@ const ContactForm = () => {
     setMessage("");
   };
 
-  const submit = () => {
+  const submit = async (e) => {
+    e.preventDefault();
+
+    console.log("User ID: ", userID);
+    console.log("SERVICE: ", ejsSERVICE);
+    console.log("TEMPLATE: ", ejsTEMPLATE);
+
+    const res = await emailjs.sendForm(
+      ejsSERVICE,
+      ejsTEMPLATE,
+      e.target,
+      userID
+    );
+    if (!res.ok) {
+      console.log("Contact Form EmailJS res failure: ", res.text);
+    } else {
+      console.log("Contact Form EmailJS res success: ", res.text);
+    }
+
     console.log("Submit Pressed");
     clearForm();
   };
 
   return (
-    <div className="contact-form">
-      <div>
+    <div className="contact_form-container">
+      <form className="contact_form" onSubmit={(e) => submit(e)}>
+        <div>
+          <input
+            id="contact_firstname"
+            name="firstname"
+            value={firstname}
+            onChange={handleFirstname}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            id="contact_lastname"
+            name="lastname"
+            value={lastname}
+            onChange={handleLastname}
+            type="text"
+            placeholder="Last Name"
+          />
+        </div>
         <input
-          id="contact_firstname"
-          value={firstname}
-          onChange={handleFirstname}
-          type="text"
-          placeholder="First Name"
+          id="contact_email"
+          name="email"
+          value={email}
+          onChange={handleEmail}
+          type="email"
+          placeholder="Email"
         />
-        <input
-          id="contact_lastname"
-          value={lastname}
-          onChange={handleLastname}
-          type="text"
-          placeholder="Last Name"
+        <textarea
+          id="contact_message"
+          name="message"
+          value={message}
+          onChange={handleMessage}
+          placeholder="Your Message"
         />
-      </div>
-      <input
-        id="contact_email"
-        value={email}
-        onChange={handleEmail}
-        type="email"
-        placeholder="Email"
-      />
-      <textarea
-        id="contact_message"
-        value={message}
-        onChange={handleMessage}
-        placeholder="Your Message"
-      />
-      <div>
-        <motion.button whileHover={{ scale: 1.2 }} onClick={submit}>
-          Submit
-        </motion.button>
-        <motion.button whileHover={{ scale: 1.2 }} onClick={clearForm}>
-          Clear Form
-        </motion.button>
-      </div>
+        <div>
+          <motion.button whileHover={{ scale: 1.2 }} type="submit" value="Send">
+            Submit
+          </motion.button>
+          <motion.button whileHover={{ scale: 1.2 }} onClick={clearForm}>
+            Clear Form
+          </motion.button>
+        </div>
+      </form>
     </div>
   );
 };
