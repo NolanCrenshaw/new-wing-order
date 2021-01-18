04fad35753fd199504d6fb0e492a4d97101f5a8b
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-18 13:11:38 -0500

    delayed elipse appearance. blur bug. named app container

--- nwo-client/src/components/Main.js
+++ nwo-client/src/components/Main.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { motion } from "framer-motion";
-import ReactModal from "react-modal";
+// import ReactModal from "react-modal";
 
-import Modal from "./Modal";
+// import Modal from "./Modal";
 import Landing from "./Landing";
 import Location from "./Location";
 import Menu from "./Menu";
@@ -14,27 +14,27 @@ import Contact from "./Contact";
 import Footer from "./Footer";
 import Admin from "./Admin";
 
-const modalStyles = {
-  content: {
-    width: "80%",
-    height: "100%",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    backgroundColor: "white",
-    border: "none",
-    boxShadow:
-      "0 3rem 5rem rgba(0,0,0,0.5), 5rem 5rem 25rem rgba(0,0,0,0.2), inset 1rem 1rem 1rem rgba(255,255,255,0.2)",
-    scrollbarWidth: "none",
-  },
-  overlay: { zIndex: 10 },
-};
+// const modalStyles = {
+//   content: {
+//     width: "80%",
+//     height: "100%",
+//     top: "50%",
+//     left: "50%",
+//     right: "auto",
+//     bottom: "auto",
+//     marginRight: "-50%",
+//     transform: "translate(-50%, -50%)",
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "flex-start",
+//     backgroundColor: "white",
+//     border: "none",
+//     boxShadow:
+//       "0 3rem 5rem rgba(0,0,0,0.5), 5rem 5rem 25rem rgba(0,0,0,0.2), inset 1rem 1rem 1rem rgba(255,255,255,0.2)",
+//     scrollbarWidth: "none",
+//   },
+//   overlay: { zIndex: 10 },
+// };
 
 const Main = () => {
   // Modal State
@@ -50,7 +50,7 @@ const Main = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 2 }}
+      transition={{ duration: 1.5 }}
       className="main-container"
     >
       {/* <ReactModal
