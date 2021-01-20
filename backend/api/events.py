6e98a29d727d7b7c1dc50647503d75d75c9c64fd
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-20 15:38:18 -0500

    env change

--- backend/api/events.py
+++ backend/api/events.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from ..models import db, Event
 
-key = os.environ.get("GOOGLE_API_KEY")
+key = os.environ["GOOGLE_API_KEY"]
 
 
 event = Blueprint("events", __name__)
