989a49ed748f7b94abf7809e12828a5ca49aed73
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-02-26 10:07:05 -0500

    form work incomplete

--- nwo-client/src/components/forms/EventForm.js
+++ nwo-client/src/components/forms/EventForm.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { DateTime } from "luxon";
-import DatePicker from "react-modern-calendar-datepicker";
-import "react-modern-calendar-datepicker/lib/DatePicker.css";
+
+import DateInput from "./DateInput";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+// import DatePicker from "react-modern-calendar-datepicker";
+// import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import TimeKeeper from "react-timekeeper";
 import { BASE_URL } from "../../config";
 
@@ -21,6 +27,13 @@ const content = {
       type: "text",
     },
   ],
+  dateTime: [
+    {
+      label: "Select Date",
+      name: "date",
+      type: "text",
+    },
+  ],
 };
 
 const schema = yup.object().shape({
@@ -34,8 +47,14 @@ const EventForm = () => {
   // Data State
   const [submittedData, setSubmittedData] = useState({});
 
+  // Controlled Component State
+  const [dateVal, setDateVal] = useState(new Date());
+
+  // Controlled Component Handlers
+  const dateValHandler = (e) => setDateVal(e.target.value);
+
   // React Hook Form Setup w/ Yup Validation
-  const { register, handleSubmit, errors } = useForm({
+  const { register, handleSubmit, errors, control } = useForm({
     mode: "onBlur",
     resolver: yupResolver(schema),
   });
@@ -47,68 +66,68 @@ const EventForm = () => {
     e.target.reset();
   };
 
-  // State
-  const [location, setLocation] = useState("");
-  const [address, setAddress] = useState("");
-  const [dateChoice, setDateChoice] = useState(null);
-  const [startTime, setStartTime] = useState(DateTime.local());
-  const [endTime, setEndTime] = useState(DateTime.local());
+  // // State
+  // const [location, setLocation] = useState("");
+  // const [address, setAddress] = useState("");
+  // const [dateChoice, setDateChoice] = useState(new Date());
+  // const [startTime, setStartTime] = useState(DateTime.local());
+  // const [endTime, setEndTime] = useState(DateTime.local());
 
-  // Input Handlers
-  const updateLocation = (e) => setLocation(e.target.value);
-  const updateAddress = (e) => setAddress(e.target.value);
+  // // Input Handlers
+  // const updateLocation = (e) => setLocation(e.target.value);
+  // const updateAddress = (e) => setAddress(e.target.value);
 
   // Restore Default States
-  const stateRestore = () => {
-    setLocation("");
-    setAddress("");
-    setDateChoice(null);
-    setStartTime(DateTime.local());
-    setEndTime(DateTime.local());
-  };
+  // const stateRestore = () => {
+  //   setLocation("");
+  //   setAddress("");
+  //   setDateChoice(null);
+  //   setStartTime(DateTime.local());
+  //   setEndTime(DateTime.local());
+  // };
 
-  const submitEvent = async (e) => {
-    e.preventDefault();
+  // const submitEvent = async (e) => {
+  //   e.preventDefault();
 
-    const token = window.localStorage.getItem("auth_token");
-    const splitStartTime = startTime.split(":");
-    const splitEndTime = endTime.split(":");
+  //   const token = window.localStorage.getItem("auth_token");
+  //   const splitStartTime = startTime.split(":");
+  //   const splitEndTime = endTime.split(":");
 
-    const event_obj = {
-      location: location,
-      address: address,
-      startTime: DateTime.fromObject({
-        year: dateChoice.year,
-        month: dateChoice.month,
-        day: dateChoice.day,
-        hour: splitStartTime[0],
-        minute: splitStartTime[1],
-      }).toISO(),
-      endTime: DateTime.fromObject({
-        year: dateChoice.year,
-        month: dateChoice.month,
-        day: dateChoice.day,
-        hour: splitEndTime[0],
-        minute: splitEndTime[1],
-      }).toISO(),
-    };
+  //   const event_obj = {
+  //     location: location,
+  //     address: address,
+  //     startTime: DateTime.fromObject({
+  //       year: dateChoice.year,
+  //       month: dateChoice.month,
+  //       day: dateChoice.day,
+  //       hour: splitStartTime[0],
+  //       minute: splitStartTime[1],
+  //     }).toISO(),
+  //     endTime: DateTime.fromObject({
+  //       year: dateChoice.year,
+  //       month: dateChoice.month,
+  //       day: dateChoice.day,
+  //       hour: splitEndTime[0],
+  //       minute: splitEndTime[1],
+  //     }).toISO(),
+  //   };
 
-    const res = await fetch(`${BASE_URL}/api/events/`, {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(event_obj),
-    });
-    if (!res.ok) {
-      // -- TODO -- handling
-      console.log("submitEvent res failure", res);
-    } else {
-      stateRestore();
-    }
-  };
+  //   const res = await fetch(`${BASE_URL}/api/events/`, {
+  //     method: "POST",
+  //     mode: "cors",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //     body: JSON.stringify(event_obj),
+  //   });
+  //   if (!res.ok) {
+  //     // -- TODO -- handling
+  //     console.log("submitEvent res failure", res);
+  //   } else {
+  //     stateRestore();
+  //   }
+  // };
 
   // Render
   return (
@@ -118,7 +137,7 @@ const EventForm = () => {
         {content.inputs.map((input, key) => {
           return (
             <div className="form_element" key={key}>
-              <div>
+              <div className="form_label-box">
                 <label>{input.label}</label>
                 <p>{errors[input.name]?.message}</p>
               </div>
@@ -131,23 +150,46 @@ const EventForm = () => {
             </div>
           );
         })}
-        <label>Select Date</label>
         <DatePicker
-          value={dateChoice}
-          onChange={setDateChoice}
-          inputPlaceholder="Select a day"
-          shouldHighlightWeekends
+          name="date"
+          selected={dateVal}
+          onSelect={(date) => setDateVal(date)}
+          onChange={(date) => setDateVal(date)}
         />
+        {/* {content.dateTime.map((input, key) => {
+          return (
+            <div className="form_element" key={key}>
+              <div className="form_label-box">
+                <label>{input.label}</label>
+                <p>{errors[input.name]?.message}</p>
+              </div>
+              <Controller
+                name={input.name}
+                value={dateVal}
+                control={control}
+                onChange={dateValHandler}
+                defaultValue={dateVal}
+                type={input.type}
+                // ref={register}
+                render={(
+                  { onChange, onBlur, value, name, ref },
+                  { invalid, isTouched, isDirty }
+                ) => (
+                  <DatePicker
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    ref={register}
+                  />
+                )}
+              />
+            </div>
+          );
+        })} */}
       </form>
       {/* <div className="form-inputs">
         <div className="event-datebox">
           <label>Select Date</label>
-          <DatePicker
-            value={dateChoice}
-            onChange={setDateChoice}
-            inputPlaceholder="Select a day"
-            shouldHighlightWeekends
-          />
         </div>
         <div className="event-timebox">
           <div className="event-timebox_timepick-container">
@@ -170,7 +212,7 @@ const EventForm = () => {
           </div>
         </div>
       </div> */}
-      <button id="create_event-button" onClick={submitEvent}>
+      <button id="create_event-button" type="submit">
         Create Event
       </button>
     </div>
